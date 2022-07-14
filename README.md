4db6f02f2343d2a221c1fc020e32368130f924b1
RohitKS7 <rohitkumarsuman2011india@gmail.com>
2022-07-14 10:09:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-todoslist-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5j4kjf)
---
+++ src/compenents/todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const todo = () => {
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div"></div>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default todo;
